8b2a6065e7549066d806709ff6671edc50377153
tangliangdong <tld@betahouse.us>
2019-12-26 09:30:01 +0800

    springcloud gateway

---
+++ springcloud-gateway/src/test/java/com/xiaotang/springcloudgateway/SpringcloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaotang.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
